3c50427329ef63b4c3bd8ed92c4f892df312bf8b
seanlandsman <sean@thelandsmans.com>
2021-09-14 15:03:42 +0100

    Add deployment script

--- scripts/deployFramework.sh
+++ scripts/deployFramework.sh
@@ -70,17 +70,23 @@ checkFileExists $SSH_LOCATION
 # we'll move this file up one in the next step
 echo "Copying zipped deployment to ag-grid"
 curl --netrc-file $CREDENTIALS_LOCATION --ftp-create-dirs -T $FILENAME ftp://ag-grid.com/ && echo "deployment file copied to ag-grid"
-
-# move file from the archives dir to the framework landing page
-echo "Moving deployment file from archives to root directory"
-ssh -i $SSH_LOCATION ceolter@ag-grid.com "mv public_html/archive/$FILENAME ./" && echo "deployment file moved to root directory"
-
-if [ -d /home/ceolter/$FILENAME ]
+if [ -d "mv public_html/archive/$FILENAME" ]
 then
     echo "File exists!!!!"
 else
   echo "File doesn't exists????"
 fi
+
+# move file from the archives dir to the framework landing page
+#echo "Moving deployment file from archives to root directory"
+#ssh -i $SSH_LOCATION ceolter@ag-grid.com "mv public_html/archive/$FILENAME ./" && echo "deployment file moved to root directory"
+
+#if [ -d /home/ceolter/$FILENAME ]
+#then
+#    echo "File exists!!!!"
+#else
+#  echo "File doesn't exists????"
+#fi
 
 # clear out old contents
 #echo "Clearing out contents of $FRAMEWORK-grid.ag-grid.com"
