e82ac5d17051c839ea14ff4f251dc6174625298f
seanlandsman <sean@thelandsmans.com>
2021-09-14 14:43:45 +0100

    Add deployment script

--- scripts/deployFramework.sh
+++ scripts/deployFramework.sh
@@ -27,7 +27,9 @@ function checkFileExists {
 # zip contents to be copied over and deployed
 DATE=`date +%Y%m%d`
 FILENAME=deployment_"$FRAMEWORK"_$DATE.zip
-zip -FSr $FILENAME "$FRAMEWORK-grid"
+cd "$FRAMEWORK-grid"
+zip -FSr ../$FILENAME *
+cd ..
 
 # $2 is optional skipWarning argument
 if [ "$2" != "skipWarning" ]; then
@@ -75,5 +77,5 @@ ssh -i $SSH_LOCATION ceolter@ag-grid.com "mv public_html/archive/$FILENAME ./"
 ssh -i $SSH_LOCATION ceolter@ag-grid.com "rm -rf /home/ceolter/$FRAMEWORK-grid.ag-grid.com/*"
 
 # unzip new contents
-unzip "/home/ceolter/$FILENAME" -d /home/ceolter/$FRAMEWORK-grid.ag-grid.com/
+unzip /home/ceolter/$FILENAME -d /home/ceolter/$FRAMEWORK-grid.ag-grid.com/
 
